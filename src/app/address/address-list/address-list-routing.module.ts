3522822edefc503ab822423be94035e2b3037b53
evelin-picazo <evepikaso@gmail.com>
2020-06-06 11:03:27 -0500

    asdf

--- src/app/address/address-list/address-list-routing.module.ts
+++ src/app/address/address-list/address-list-routing.module.ts
@@ -3,10 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddressListComponent} from './address-list.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AddressListComponent
-  }
+
 ];
 
 @NgModule({
