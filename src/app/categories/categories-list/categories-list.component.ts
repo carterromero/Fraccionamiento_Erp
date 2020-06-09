23617bebd6fd2a7045db3e60c7346f5b8a1b2457
kaledkeyter <44480645+kaledkeyter@users.noreply.github.com>
2020-06-09 11:20:46 -0500

    Create categories-list.component.ts

---
+++ src/app/categories/categories-list/categories-list.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/categories.service';
+import { Router } from '@angular/router';
+import { Categories } from 'src/app/categories';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-categories-list',
+  templateUrl: './categories-list.component.html',
+  styleUrls: ['./categories-list.component.scss']
+})
+export class CategoriesListComponent implements OnInit {
+
+  general: Observable<Categories[]>;
+
+  constructor(private generalService: CategoriesService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+      
+      this.reloadData();
+  }
+  
+  reloadData() {
+    
+    this.generalService.getEmployeeList().subscribe(
+      data => {
+        console.log(data);
+        this.general = this.generalService.getEmployeeList();
+      },
+      error => {
+        console.log(error);
+        //localStorage.setItem('token', "");
+        //this.router.navigate(['login']);     
+      });
+  }
+
+  deleteGeneral(id: number) {
+    this.generalService.deleteEmployee(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => {console.log(error);
+        //localStorage.setItem('token', "");
+        //this.router.navigate(['login']); 
+        }
+      );
+  }
+
+  generalDetails(id: number){
+    this.router.navigate(['general-details', id]);
+  }
+
+  updateGeneral(id: number){
+    this.router.navigate(['update-general', id]);
+  }
+
+}
