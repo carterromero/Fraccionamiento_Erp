e3c2cf624cd58a8553ea6cbe5f156cb87d14bf32
kaledkeyter <44480645+kaledkeyter@users.noreply.github.com>
2020-06-09 11:20:27 -0500

    Create categories.service.ts

---
+++ src/app/categories.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  private baseUrl = 'https://dtipruebas-idt2tecbgzl5-ia.integration.ocp.oraclecloud.com:443/';
+  private username='abdiel.romero@dti-consultores.com';
+  private password='Ingenieroars07';
+  
+
+  constructor(private http: HttpClient) { }
+
+  createEmployee(employee: Object): Observable<Object> {
+    const headers=new HttpHeaders({Authorization:'Basic ' + btoa(this.username+ ':' +this.password)})
+    return this.http.post(`${this.baseUrl}ic/api/integration/v1/flows/rest/ERP_INV_INSERT_CATEGORI/1.0/categories`, employee,{headers});
+  }
+
+  getEmployee(id: number): Observable<any> {
+    const headers=new HttpHeaders({Authorization:'Basic ' + btoa(this.username+ ':' +this.password)})
+    return this.http.get(`${this.baseUrl}ic/api/integration/v1/flows/rest/ERP_INV_GET_ONE_CATEGO/1.0/categories/{id}`,{headers});
+  }
+
+  updateEmployee(id: number, value: any): Observable<Object> {
+    const headers=new HttpHeaders({Authorization:'Basic ' + btoa(this.username+ ':' +this.password)})
+    return this.http.put(`${this.baseUrl}ic/api/integration/v1/flows/rest/ERP_INV_UPDATE_CATEGORI/1.0/categories/{id}`, value,{headers});
+  }
+
+  deleteEmployee(id: number): Observable<any> {
+    const headers=new HttpHeaders({Authorization:'Basic ' + btoa(this.username+ ':' +this.password)})
+    return this.http.delete(`${this.baseUrl}ic/api/integration/v1/flows/rest/ERP_INV_DELETE_CATEGORI/1.0/categories/{id}`, { responseType: 'text', headers});
+  }
+
+  getEmployeeList(): Observable<any> {
+    const headers=new HttpHeaders({Authorization:'Basic ' + btoa(this.username+ ':' +this.password)})
+    return this.http.get(`${this.baseUrl}ic/api/integration/v1/flows/rest/ERP_INV_GET_ALL_CATEGO/1.0/categories`,{headers});
+  }
+}
