97c7cf685c42e6b6dc3238e562043d63c13db12b
evelin-picazo <evepikaso@gmail.com>
2020-06-09 10:06:10 -0500

    reports

--- src/app/theme/layout/admin/navigation/navigation.ts
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -335,7 +335,7 @@ const NavigationItems = [
       },
       {
         id: 'po-reporte',
-        title: 'Reportes',
+        title: 'Reportes Proveedores',
         type: 'collapse',
         icon: 'feather icon-monitor',
         children: [
@@ -349,8 +349,23 @@ const NavigationItems = [
 
                                 
         ]
-      },
+      }, {
+        id: 'po-reporte',
+        title: 'Reportes Ordenes de compra',
+        type: 'collapse',
+        icon: 'feather icon-monitor',
+        children: [
+          {
+            id: 'Reports',
+            title: 'Reportes',
+            type: 'item',            
+            url: '/purchase_orders-list'
+            
+          }
 
+                                
+        ]
+      }
 
 
     ]
