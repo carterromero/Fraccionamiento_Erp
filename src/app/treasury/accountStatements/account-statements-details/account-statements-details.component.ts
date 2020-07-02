8525303c78a14d58fde3c905fb06d7607a504c68
obedGalicia100 <66484234+obedGalicia100@users.noreply.github.com>
2020-07-02 03:47:21 -0500

    .....

---
+++ src/app/treasury/accountStatements/account-statements-details/account-statements-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-statements-details',
+  templateUrl: './account-statements-details.component.html',
+  styleUrls: ['./account-statements-details.component.scss']
+})
+export class AccountStatementsDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/treasury/accountStatements/account-statements-list/account-statements-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-statements-list',
+  templateUrl: './account-statements-list.component.html',
+  styleUrls: ['./account-statements-list.component.scss']
+})
+export class AccountStatementsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
