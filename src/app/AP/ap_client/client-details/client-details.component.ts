c2790327c5d97def3f77f40916e1394223927df7
andrescoyotlcortes <acoycor@gmail.com>
2020-07-16 17:59:29 -0500

    detalles

--- src/app/AP/ap_client/client-details/client-details.component.ts
+++ src/app/AP/ap_client/client-details/client-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Client } from 'src/app/client';
+import { ClientService } from 'src/app/client.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-client-details',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientDetailsComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+ 
+  id: number;
+  employee: Client;  
+  alertDisable = true;
+  alertMessage = "null";
+
+  constructor(private route: ActivatedRoute,private router: Router,
+    private employeeService: ClientService) { }
+
+  ngOnInit() {
+    this.employee = new Client();    
+    this.id = this.route.firstChild.snapshot.params['id']
+    console.log(this.id);
+    
+    
+    this.employeeService.getEmployee(this.id)
+      .subscribe(data => {
+        console.log(data);
+        this.employee = data;
+      }, error => {
+        console.log(error);
+        let coins = [];
+        for (let key in error) {
+          this.alertDisable = false;
+          this.alertMessage = error['statusText'];          
+        }
+      });
   }
-
 }
