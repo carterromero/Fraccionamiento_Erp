b35335691a2f11497f9df15dccf58eb0aee04c13
Idania <idania.gonz.reyes@gmail.com>
2020-07-17 21:13:40 -0500

    TS-Manage-banks

--- src/app/treasury/manageBaks/create-manage-banks/create-manage-banks.component.ts
+++ src/app/treasury/manageBaks/create-manage-banks/create-manage-banks.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ManageBanks } from '../../../manageBank';
 import { Router } from '@angular/router';
 import { ManageBanksService } from '../../../manage-banks.service';
-import { BankCodes } from '../../../bankCodes';
-import { BankCodesService } from '../../../bank-codes.service';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-create-manage-banks',
@@ -14,15 +12,14 @@ import { Observable } from 'rxjs';
 export class CreateManageBanksComponent implements OnInit {
 
   employee: ManageBanks = new ManageBanks();
-  codes: Observable<BankCodes[]>;
   submitted = false;
 
   constructor(private employeeService: ManageBanksService,
-    private router: Router, private bankcodesService:BankCodesService) { }
+    private router: Router) { }
 
   ngOnInit() {
     //code
-    this.reloadData();
+ 
   }
 
   newEmployee(): void {
@@ -30,21 +27,7 @@ export class CreateManageBanksComponent implements OnInit {
     this.employee = new ManageBanks();
   }
 
-  reloadData() {
-    
-    this.bankcodesService.getEmployeeList().subscribe(
-      data => {
-        console.log(data);
-        this.codes = this.bankcodesService.getEmployeeList();
-      },
-      error => {
-        console.log(error);
-        //localStorage.setItem('token', "");
-        //this.router.navigate(['login']);     
-      });
-
-      
-  }
+  
   save() {
 
     this.employee.created_by= Number(localStorage.getItem('id'));
