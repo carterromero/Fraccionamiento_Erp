6d163bba708aa6e8f510adebb57457ccfaa93267
andrescoyotlcortes <acoycor@gmail.com>
2020-07-20 17:54:46 -0500

    mensaje

--- src/app/AP/ap_billtopay/create-billtopay/create-billtopay.component.ts
+++ src/app/AP/ap_billtopay/create-billtopay/create-billtopay.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+
 import { Observable } from 'rxjs';
 import { Billtopay } from 'src/app/billtopay';
 import { Billtopayservice } from 'src/app/billtopay.service';
 import { PurcharseService } from 'src/app/purcharse.service';
 import { Purcharse } from 'src/app/purcharse';
+import { BankAccounts } from '../../../bankAccounts';
+import { BankAccountsService } from '../../../bank-accounts.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Creditor } from 'src/app/creditor';
+import { CreditorService } from 'src/app/creditor.service';
 
 @Component({
   selector: 'app-create-billtopay',
@@ -12,21 +17,30 @@ import { Purcharse } from 'src/app/purcharse';
   styleUrls: ['./create-billtopay.component.scss']
 })
 export class CreateBilltopayComponent implements OnInit {
-
+  creditor: Observable<BankAccounts[]>;
   employee: Billtopay = new Billtopay();
   purcharse: Observable<Purcharse[]>;
+  general: Observable<Billtopay[]>;
+  credi: Observable<Creditor[]>;
+  credito:Creditor = new Creditor();
+name : string;
   alertDisable = true;
   alertDisables = true;
   alertMessage = "null";
   alertMessages = "null";
   datos:String;
-
+  creditor_business_name:String;
+  cliente_name_cliente :String;
   constructor(private employeeService: Billtopayservice,
     private purcharseService: PurcharseService,
+    private bankAccountsService: BankAccountsService,
+    private employeesService: CreditorService,
+    private route: ActivatedRoute,
     private router: Router) { }
 
   ngOnInit() {
     this.reloadDatas();
+    this.reloadDatas1();
   }
 
   reloadDatas() 
@@ -47,10 +61,60 @@ export class CreateBilltopayComponent implements OnInit {
     );      
   }
 
+ 
+  
+
+  reloadData2() {
+    this.name = this.route.firstChild.snapshot.params['name']
+
+      console.log(this.name);
+      this.employeesService.getEmployeed(this.employee.creditor_business_name)
+        .subscribe(data2 => {
+          console.log(data2);
+          this.employee = data2;
+        });
+   
+  } 
+
+  reloadData3() {
+    this.name = this.route.firstChild.snapshot.params['name']
+    console.log(this.name);
+    this.employeeService.getEmployeecl(this.employee.cliente_name_cliente)
+      .subscribe(data3 => {
+        console.log(data3);
+        this.employee = data3;
+      });
+    
+} 
+
+reloadDatas1() 
+{
+
+  this.employeesService.getEmployeeList().subscribe(
+    data => {
+      console.log(data);
+      this.credi= this.employeesService.getEmployeeList();
+      console.log(this.creditor_business_name);
+      
+      if (this.creditor_business_name == this.employee.creditor_business_name )
+      this.reloadData2()
+    },
+    error => {
+      console.log(error);
+      let coins = [];
+      for (let key in error) {
+        this.alertMessage = error['statusText'];          
+      }
+    }
+  );      
+}
+
+
+
+
   newEmployee(): void {
     this.employee = new Billtopay();
   }
-
 
 
   handleUpload(event) {
@@ -94,27 +158,13 @@ export class CreateBilltopayComponent implements OnInit {
       });
   }
 
-  onSubmit() 
-  {
-
-  this.alertDisable = true;
-  this.alertDisables = true;
-
-  if(this.employee.billtopay_invoice_folio == "" ||  this.employee.billtopay_invoice_folio ==null ){
-    this.alertDisable = false;
-    this.alertMessage = "campo Incompleto o repetido";          
-  }
-
-
-
-  else{
-    this.save();    
-  }
-  }
+ 
 
   gotoList() 
   {
     this.router.navigate(['billtopay-list']);
   }
+
+
 
 }
