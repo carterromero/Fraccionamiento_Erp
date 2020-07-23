9e3db446c190bdadd29d7e0abd65da20a5126dd2
ivandominguez-dti <65007007+ivandominguez-dti@users.noreply.github.com>
2020-07-23 16:06:46 -0500

    conciliacionprueba2

--- src/app/AR/ar-conciliacion/create-conciliacion-ar/create-conciliacion-ar.component.ts
+++ src/app/AR/ar-conciliacion/create-conciliacion-ar/create-conciliacion-ar.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Conciliacionar } from 'src/app/conciliacion-ar';
-import { ConciliacionARService } from 'src/app/Conciliacion-ar.service';
-import { Collection } from 'src/app/Collection';
-import { CollectionService } from 'src/app/collection.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Conciliacion } from 'src/app/conciliacion';
+import { Observable } from 'rxjs';
+import { ConciliacionService } from 'src/app/conciliacion.service';
+import { PaymentRecord } from 'src/app/payment-record';
+import { PaymentRecordService } from 'src/app/payment-record.service';
 
-import { BankAccountsService } from 'src/app/bank-accounts.service';
-import { BankAccounts } from 'src/app/bankAccounts';
+import { LinesService } from "src/app/services/gl/lines.service";
+import { Lines } from "src/app/services/gl/lines";
 
-import { Observable, from } from 'rxjs';
 @Component({
   selector: 'app-create-conciliacion-ar',
   templateUrl: './create-conciliacion-ar.component.html',
@@ -16,28 +16,33 @@ import { Observable, from } from 'rxjs';
 })
 export class CreateConciliacionARComponent implements OnInit {
 
-  employee: Conciliacionar = new Conciliacionar();
-  Collection: Observable<Collection[]>;
+
+  employee: Conciliacion = new Conciliacion();
+  purcharses: Observable<PaymentRecord[]>;
+  purcharsess: Observable<Lines[]>;
   alertDisable = true;
   alertDisables = true;
   alertMessage = "null";
   alertMessages = "null";
 
   constructor(
-    private employeeService: ConciliacionARService,
-    private CollectionService: CollectionService,
+    private employeeService: ConciliacionService,
+    private paymentRecordService: PaymentRecordService,
+    private accountsService: LinesService,
     private router: Router) { }
 
   ngOnInit() {
+
     this.reloadDatas();
   }
 
+
   reloadDatas() {
 
-    this.CollectionService.getEmployeeList().subscribe(
+    this.paymentRecordService.getEmployeeList().subscribe(
       data => {
         console.log(data);
-        this.Collection = this.CollectionService.getEmployeeList();
+        this.purcharses = this.paymentRecordService.getEmployeeList();
       },
       error => {
         console.log(error);
@@ -47,32 +52,39 @@ export class CreateConciliacionARComponent implements OnInit {
         }
       }
     );
-
-
-
-
+    this.accountsService.getEmployeeList().subscribe(
+      data => {
+        console.log(data);
+        this.purcharsess = this.accountsService.getEmployeeList();
+      },
+      error => {
+        console.log(error);
+        let coins = [];
+        for (let key in error) {
+          this.alertMessage = error['statusText'];
+        }
+      }
+    );
   }
 
   newEmployee(): void {
-    this.employee = new Conciliacionar();
+    this.employee = new Conciliacion();
   }
 
   save() {
 
-    /// this.employee.user_id="3";
-    
-    console.log( localStorage.getItem('condominums'));
-    this.employee.concilitiation_condominums_id =Number( localStorage.getItem('condominums'));
+    this.employeeService.createEmployee(this.employee)
+    this.employee.concilitiation_condominums_id = localStorage.getItem('condominums');
+    this.employee.concilitiation_document_type="Poliza";
+    this.employee.created_by=Number(localStorage.getItem('id'));
     this.employee.concilitiation_origin="AR";
-    this.employee.created_by = Number( localStorage.getItem('id'));
 
-    
     this.employeeService.createEmployee(this.employee)
       .subscribe(data => {
         console.log(data);
         this.alertDisables = false;
-        this.alertMessages = "Se inserto la acredor correctamente";
-        this.employee = new Conciliacionar();
+        this.alertMessages = "Se inserto  correctamente";
+        this.employee = new Conciliacion();
       },
         error => {
           console.log(error);
@@ -89,29 +101,20 @@ export class CreateConciliacionARComponent implements OnInit {
     this.alertDisable = true;
     this.alertDisables = true;
 
-    /* if (this.employee. == "" || this.employee.creditor_business_name == null) {
+    if (this.employee.concilitiation_origin == "" || this.employee.concilitiation_origin == null) {
       this.alertDisable = false;
       this.alertMessage = "Nombre Incompleto";
     }
 
-    else if (this.employee.creditor_addres == "" || this.employee.creditor_addres == null) {
-      this.alertDisable = false;
-      this.alertMessage = "Dirección Incompleta";
-    }
-
-    else if (this.employee.creditor_turn == "" || this.employee.creditor_turn == null) {
-      this.alertDisable = false;
-      this.alertMessage = "giro Incompleta";
-    }
 
 
     else {
       this.save();
-    } */
+    }
   }
 
   gotoList() {
-    this.router.navigate(['collection-list']);
+    this.router.navigate(['conciliacionp-list']);
   }
 
 }
