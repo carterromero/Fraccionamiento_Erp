4e0c6c5db0b9896d8b9dfe3c2cb080a2df63e31f
andrescoyotlcortes <acoycor@gmail.com>
2020-07-27 16:56:11 -0500

    poliza

--- src/app/gl/catalogs/journals/create-journals/create-journals.component.ts
+++ src/app/gl/catalogs/journals/create-journals/create-journals.component.ts
@@ -2,34 +2,57 @@ import { Component, OnInit } from '@angular/core';
 import { JournalsService } from "../../../../services/gl/journals.service";
 import { Journals } from "../../../../services/gl/journals";
 import { Router } from '@angular/router';
-
+import { PeriodsService } from "../../../../services/gl/periods.service";
+import { Periods } from "../../../../services/gl/periods";
+import { Observable } from "rxjs";
+import { Directive, ElementRef, HostListener, Input } from '@angular/core'; 
 @Component({
   selector: 'app-create-journals',
   templateUrl: './create-journals.component.html',
+  
   styleUrls: ['./create-journals.component.scss']
 })
 export class CreateJournalsComponent implements OnInit {
 
 
   employee: Journals = new Journals();
+  periods: Observable<Periods[]>;
   alertDisable = true;
   alertDisables = true;
   alertMessage = "null";
   alertMessages = "null";
-
+  @Input() OnlyNumber: boolean; 
   constructor(private employeeService: JournalsService,
+    private periodsService: PeriodsService,
+    private el: ElementRef,
     private router: Router) { }
 
   ngOnInit() {
+    this.reloadDatas1() 
   }
 
   newEmployee(): void {
     this.employee = new Journals();
   }
 
-  save() {
+  reloadDatas1() {
+    this.periodsService.getPeriodsList().subscribe(
+      data => {
+        console.log(data);
+        this.periods = this.periodsService.getPeriodsList();
+      },
+      error => {
+        console.log(error);
+        //localStorage.setItem('token', "");
+        //this.router.navigate(['auth/signin']);     
+      });
+  }
 
+  save() {
+    
     this.employee.user_id="3";
+    this.employee.created_by = localStorage.getItem('id');
+    this.employee.journals_condominiums_id = localStorage.getItem('condominums');
     this.employeeService.createEmployee(this.employee)
       .subscribe(data => 
         {
@@ -81,6 +104,33 @@ export class CreateJournalsComponent implements OnInit {
   {
     this.router.navigate(['journals-list']);
   }
+
+
+  @HostListener('keydown', ['$event']) onKeyDown(event) { 
+    let e = <KeyboardEvent> event; 
+    if (this.OnlyNumber) { 
+     if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 || 
+     // Allow: Ctrl+A 
+     (e.keyCode === 65 && (e.ctrlKey || e.metaKey)) || 
+     // Allow: Ctrl+C 
+     (e.keyCode === 67 && (e.ctrlKey || e.metaKey)) || 
+     // Allow: Ctrl+V 
+     (e.keyCode === 86 && (e.ctrlKey || e.metaKey)) || 
+     // Allow: Ctrl+X 
+     (e.keyCode === 88 && (e.ctrlKey || e.metaKey)) || 
+     // Allow: home, end, left, right 
+     (e.keyCode >= 35 && e.keyCode <= 39)) { 
+      // let it happen, don't do anything 
+      return; 
+     } 
+     // Ensure that it is a number and stop the keypress 
+     if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) { 
+      e.preventDefault(); 
+     } 
+     } 
+    } 
+
+
 
 }
 
