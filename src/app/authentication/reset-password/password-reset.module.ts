62e5fa6b1a9a05f1aafec2c6f476b97d4b6afa5b
Idania <idania.gonz.reyes@gmail.com>
2020-07-29 16:26:56 -0500

    admin-reset

--- src/app/authentication/reset-password/password-reset.module.ts
+++ src/app/authentication/reset-password/password-reset.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input'
 import { ResetPasswordRoutingModule } from './password-reset-routing.module';
 import { ResetPasswordComponent } from './password-reset.component';
 import {SharedModule} from '../../theme/shared/shared.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   imports: [
     CommonModule,
     ResetPasswordRoutingModule,
-    SharedModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    ReactiveFormsModule
+    SharedModule
   ],
   declarations: [ResetPasswordComponent]
 })
