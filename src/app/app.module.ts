24a135ae805a51bd96d85a1c8a8d103a7ab35049
Idania <idania.gonz.reyes@gmail.com>
2020-08-03 16:50:58 -0500

    rh-employees

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,10 @@ import { AngularFireModule } from '@angular/fire';
 import { MessagingService } from './messaging.service';
 import { environment } from '../environments/environment';
 import { AsyncPipe } from '../../node_modules/@angular/common';
+import { CreateCommentsComponent } from './admin/releases/comments/create-comments/create-comments.component';
+import { CommentsListComponent } from './admin/releases/comments/comments-list/comments-list.component';
+import { CommentsDetailsComponent } from './admin/releases/comments/comments-details/comments-details.component';
+import { UpdateCommentsComponent } from './admin/releases/comments/update-comments/update-comments.component';
 
 
 
@@ -51,6 +55,14 @@ import { AsyncPipe } from '../../node_modules/@angular/common';
     ConfigurationComponent,
 
     ToggleFullScreenDirective,
+
+    CreateCommentsComponent,
+
+    CommentsListComponent,
+
+    CommentsDetailsComponent,
+
+    UpdateCommentsComponent,
 
 
 
