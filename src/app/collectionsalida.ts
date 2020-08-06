37837438bf727025163da338974a1960154c91ef
ivandominguez-dti <65007007+ivandominguez-dti@users.noreply.github.com>
2020-08-05 20:30:00 -0500

    updateReporte3.2

--- src/app/collectionsalida.ts
+++ src/app/collectionsalida.ts
@@ -1,13 +1,4 @@
 export class Collectionsaslida{
-    
-    collection_name_resident:string;
-    created_by:string;
-    collection_amount:string;
-    collection_collection_concept:string;
-    collection_id:number;
-    collection_status:string;
-
-
     residente :string;
     mes :string;
     monto :string;
