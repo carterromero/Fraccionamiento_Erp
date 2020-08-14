b57fd8b6ae66d137f9b9591005ac55e5c0f53632
Idania <idania.gonz.reyes@gmail.com>
2020-08-13 19:15:57 -0500

    rh-calendar

--- src/app/RH/Reservations/create-reservations/create-reservations.component.ts
+++ src/app/RH/Reservations/create-reservations/create-reservations.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TenantsService } from 'src/app/tenants.service';
 import { Tenants } from 'src/app/tenants';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -8,8 +8,9 @@ import { ReservationsService } from 'src/app/reservations.service';
 import { ArticlesService } from "src/app/articles.service";
 import { Articles } from "src/app/articles";
 import * as moment from 'moment';
-import { CalendarOptions } from '@fullcalendar/angular'; 
+import { CalendarOptions, EventApi, DateSelectArg, EventClickArg, FullCalendarComponent } from '@fullcalendar/angular'; 
 import esLocale from '@fullcalendar/core/locales/es';
+import { EventInput } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-create-reservations',
@@ -17,7 +18,7 @@ import esLocale from '@fullcalendar/core/locales/es';
   styleUrls: ['./create-reservations.component.scss']
 })
 export class CreateReservationsComponent implements OnInit {
-  
+ // @ViewChild('calendar') calendarComponent: FullCalendarComponent;
   reservation: Reservations = new Reservations();
   tenants: Observable<Tenants[]>;
   articles: Observable<Articles[]>;
@@ -32,20 +33,43 @@ export class CreateReservationsComponent implements OnInit {
   data1 = [];
   data2 = [];
   titlee = [];
+  ds: Date;
+  de:Date;
   constructor(private tenantsService:TenantsService, private route: ActivatedRoute,
   private reservationsService: ReservationsService,  private articlesService : ArticlesService, private router: Router) { }
 
   ngOnInit() {
     //code
-    this.reloadDatas();
+
+    this.articlesService.getListCommons().subscribe(
+      data => {
+        console.log(data);
+        this.articles = this.articlesService.getListCommons();
+      },
+      error => {
+        console.log(error);
+        let coins = [];
+        for (let key in error) {
+          this.alertMessage = error['statusText'];          
+        }
+      }
+    );      
+
+   // this.reloadDatas();
     this.reloadData();
     
+
     }
+
+
     reloadDataList() {
+     
+   console.log("Te activas");
       this.reservationsService.getReservationListR(this.reservation.articles_sku).subscribe(
         data => {
           console.log(data);
           this.reservations = this.reservationsService.getReservationListR(this.reservation.articles_sku);
+       //   let calendarApi = this.reservationsService.getReservationListR(this.reservation.articles_sku);
           for (let key in data) {
             var dats = data[key];
             console.log(dats);
@@ -54,10 +78,15 @@ export class CreateReservationsComponent implements OnInit {
               this.data1.push(
                 [dats['reservations_start']]);
                 this.data2.push(
-                  [dats['reservations_end']]);
-                  console.log(this.titlee);
-                  console.log(this.data1);
-                  console.log(this.data2);
+                  [dats['reservations_end']]); 
+                /*  this.ds=this.data1[key];
+                  this.ds=moment(this.ds).format('2020-08-24T18:00:00Z'); 
+                  this.de=this.data2[];
+                  console.log(this.ds);*/
+                  console.log(this.titlee[0]);
+                  console.log(this.data1[0]);
+                  console.log(this.data2[0]);
+                  
           }
         },
         error => {
@@ -65,7 +94,8 @@ export class CreateReservationsComponent implements OnInit {
         });
     } 
    
-    calendarOptions: CalendarOptions = {
+
+  /*  calendarOptions: CalendarOptions = {
       headerToolbar: {
         left: 'prev,next',
         center: 'title',
@@ -74,22 +104,56 @@ export class CreateReservationsComponent implements OnInit {
       initialView: 'dayGridMonth',
       themeSystem:'Flatly',
       locale: esLocale,
-   // dateClick: this.handleDateClick.bind(this), // bind is important!
-   events:   [
-     {
-    title: this.titlee[0],
-    start: this.data1[0],
-    end:this.data1[0]
-}
-]
+   // dateClick: this.handleDateClick.bind(this), // bind is important
+   events: [
+    { title: 'PRUEBA', start:'2020-08-13T18:00:00Z', end:this.ds }
+  ]
     };
-   /* handleDateClick(arg) {
-      alert('date click! ' + arg.dateStr)
-    }*/
+*/
+
+    calendarOptions: CalendarOptions = {
+      initialView: 'dayGridMonth',
+      weekends: false, // initial value
+      
+    };
   
+    toggleWeekends() {
+
+      
+      console.log("Te activas");
+      this.reservationsService.getReservationListR(this.reservation.articles_sku).subscribe(
+        data => {
+          console.log(data);
+          this.reservations = this.reservationsService.getReservationListR(this.reservation.articles_sku);
+       //   let calendarApi = this.reservationsService.getReservationListR(this.reservation.articles_sku);
+          for (let key in data) {
+            var dats = data[key];
+            //console.log(dats);
+            this.titlee.push(
+              [dats['articles_name_article']]);
+              this.data1.push(
+                [dats['reservations_start']]);
+                this.data2.push(
+                  [dats['reservations_end']]); 
+                  this.ds=this.data2[0];
+                  console.log(this.ds);
+                  this.calendarOptions.events = [
+        { title: this.titlee[key], start:moment(this.data1[key]).format('2020-08-04T11:00:00'), end:moment(this.data2[key]).toDate() }
+      ]
+                  
+          }
+        },
+        error => {
+          console.log(error);   
+        });
+   
+    
+    
+    }
 
 
- 
+   
+    
 
   javq(event){
  
@@ -100,7 +164,7 @@ export class CreateReservationsComponent implements OnInit {
 
   
 
-  reloadDatas() 
+  /*reloadDatas() 
   {
 
     this.articlesService.getListCommons().subscribe(
@@ -116,7 +180,7 @@ export class CreateReservationsComponent implements OnInit {
         }
       }
     );      
-  }
+  }*/
 
   reloadData() 
   {
